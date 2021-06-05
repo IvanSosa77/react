56cfa579707ae491af4b91905a5dde941fc380d2
IvanSosa77 <ivandarling16@gmail.com>
2021-06-05 14:42:45 -0300

    clase 3-agregue navbar y itemlistcontainer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import image from './assets/carr.jpg';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
-    
+    <NavBar image={image}/>
+    <ItemListContainer greeting="Entregable clase 3 React Js"/>
     </div>
   );
 }
--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import {Navbar} from 'react-bootstrap'
 
-export default function NavBar() {
+
+export default function NavBar(props) {
     return (
         <Navbar bg="light">
-        <Navbar.Brand href="#home">PetShop</Navbar.Brand>
+        <Navbar.Brand href="#home">Shoes</Navbar.Brand>
         <Navbar.Brand href="#About">About</Navbar.Brand>
         <Navbar.Brand href="#Contact">Contact</Navbar.Brand>
+        <Navbar.Brand href="#home">
+      <img
+        src={props.image}
+        width="60"
+        height="50"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+    </Navbar.Brand>
       </Navbar>
     )
-}
+} 
